2303127030ab9ec120e856df34bbc4f6feb3f29d
Aom <Aaom6740@gmail.com>
2025-04-18 21:44:27 +0700

    diff

--- lib/screens/role_selection_screen.dart
+++ lib/screens/role_selection_screen.dart
@@ -4,6 +4,47 @@ import 'menu_screen.dart';
 import 'admin_menu_screen.dart';
 
 class RoleSelectionScreen extends StatelessWidget {
+  final adminPassword = '1234';
+
+  void _showPasswordDialog(BuildContext context) {
+    final passwordController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('ใส่รหัสผ่านผู้ดูแล'),
+        content: TextField(
+          controller: passwordController,
+          obscureText: true,
+          decoration: InputDecoration(hintText: 'รหัสผ่าน'),
+        ),
+        actions: [
+          TextButton(
+            child: Text('ยกเลิก'),
+            onPressed: () => Navigator.pop(context),
+          ),
+          ElevatedButton(
+            child: Text('ยืนยัน'),
+            onPressed: () {
+              if (passwordController.text == adminPassword) {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => AdminMenuScreen()),
+                );
+              } else {
+                Navigator.pop(context);
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('รหัสผ่านไม่ถูกต้อง')),
+                );
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,10 +66,7 @@ class RoleSelectionScreen extends StatelessWidget {
             ElevatedButton(
               child: Text('เข้าสู่ระบบผู้ดูแล'),
               style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurpleAccent),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => AdminMenuScreen()),
-              ),
+              onPressed: () => _showPasswordDialog(context),
             ),
           ],
         ),
