5b32f38efa486d602d883f048f22791a4ca4bb1c
Aom <Aaom6740@gmail.com>
2025-04-23 18:15:16 +0700

    ได้แล้ว

--- lib/screens/order_history_screen.dart
+++ lib/screens/order_history_screen.dart
@@ -3,56 +3,61 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../models/cart_model.dart';
 
-class OrderHistoryScreen extends StatelessWidget {
+class OrderHistoryScreen extends StatefulWidget {
+  @override
+  _OrderHistoryScreenState createState() => _OrderHistoryScreenState();
+}
+
+class _OrderHistoryScreenState extends State<OrderHistoryScreen> {
+  String selectedTable = 'โต๊ะ 1';
+
   @override
   Widget build(BuildContext context) {
     final cartModel = Provider.of<CartModel>(context);
-    final tables = List.generate(10, (index) => 'โต๊ะ ${index + 1}');
-    String? selectedTable;
+    final orders = cartModel.orderHistory;
+    final filteredOrders = orders.where((o) => o.tableNumber == selectedTable).toList();
 
-    return StatefulBuilder(
-      builder: (context, setState) => Scaffold(
-        appBar: AppBar(title: Text('ตรวจสอบคำสั่งซื้อ')),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: DropdownButton<String>(
-                value: selectedTable,
-                hint: Text('เลือกโต๊ะ'),
-                isExpanded: true,
-                items: tables
-                    .map((table) => DropdownMenuItem(
-                          value: table,
-                          child: Text(table),
-                        ))
-                    .toList(),
-                onChanged: (value) => setState(() => selectedTable = value),
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ตรวจสอบคำสั่งซื้อ'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: DropdownButton<String>(
+              value: selectedTable,
+              isExpanded: true,
+              items: List.generate(10, (i) => 'โต๊ะ ${i + 1}').map((table) {
+                return DropdownMenuItem(
+                  value: table,
+                  child: Text(table),
+                );
+              }).toList(),
+              onChanged: (value) => setState(() => selectedTable = value!),
             ),
-            Expanded(
-              child: selectedTable == null
-                  ? Center(child: Text('กรุณาเลือกโต๊ะ'))
-                  : ListView(
-                      children: cartModel.orderHistory
-                          .where((order) => order.tableNumber == 'โต๊ะ $selectedTable')
-                          .map((order) => Card(
-                                margin: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                                child: ListTile(
-                                  title: Text(order.dateTime),
-                                  subtitle: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: order.items
-                                        .map((item) => Text('- $item'))
-                                        .toList(),
-                                  ),
-                                ),
-                              ))
-                          .toList(),
-                    ),
-            )
-          ],
-        ),
+          ),
+          Expanded(
+            child: filteredOrders.isEmpty
+                ? Center(child: Text('ไม่มีรายการสั่งซื้อ'))
+                : ListView.builder(
+                    itemCount: filteredOrders.length,
+                    itemBuilder: (context, index) {
+                      final order = filteredOrders[index];
+                      return Card(
+                        margin: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                        child: ListTile(
+                          title: Text(order.dateTime),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: order.items.map((e) => Text(e)).toList(),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+          ),
+        ],
       ),
     );
   }
